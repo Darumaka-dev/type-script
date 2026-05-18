a01b6af44c651f4beec66072c7c549ddd18bdf5d
Elina Samoilenko <elina.samoilenko@epam.com>
2026-05-18 19:42:49 +0300

    feat(registration-form): add code review suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       {/* <ProductCard title={title} price={price}/> */}
-      <RegistrationForm></RegistrationForm>
+      <RegistrationForm />
     </>
   )
 }
--- src/RegistrationForm.tsx
+++ src/RegistrationForm.tsx
@@ -1,37 +1,37 @@
-import { Button, TextField, Select, MenuItem } from "@mui/material";
+import { Button, TextField, Select, MenuItem, Box, FormControl, InputLabel } from "@mui/material";
 import { useForm, Controller, type SubmitHandler } from "react-hook-form";
 
 type FormInput = {
   name: string;
   email: string;
-  password: number | string;
-  confirmPassword: number | string;
+  password: string;
+  confirmPassword: string;
   role: "user" | "business";
-  companyName: string;
+  companyName?: string;
 };
 
 export const RegistrationForm = () => {
   const {
     control,
+    register,
     handleSubmit,
     formState: { errors },
     watch,
-  } = useForm({
+  } = useForm<FormInput>({
+    shouldUnregister: true,
     defaultValues: {
       name: "",
       email: "",
-      password: undefined,
-      confirmPassword: undefined,
+      password: "",
+      confirmPassword: "",
       role: "user",
       companyName: "",
     },
   });
 
-  const passwordValue = watch("password");
-  const confirmPasswordValue = watch("confirmPassword");
+  const passwordValue = watch("password"); // useWatch()
   const roleValue = watch("role");
 
-  console.log(roleValue);
   const patternEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   const onSubmit: SubmitHandler<FormInput> = (data) => {
@@ -40,76 +40,65 @@ export const RegistrationForm = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="name"
-          control={control}
-          rules={{
-            required: "This is required.",
+       <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{
+            width: 400,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
           }}
-          render={({ field }) => (
-            <TextField
-              placeholder="Enter name"
-              sx={{ mr: 3 }}
-              helperText={errors.name && errors.name.message}
-              error={errors.name ? true : false}
-              {...field}
-            />
-          )}
+        >
+        <TextField
+          {...register("name", {
+            required: "Name is required.",
+          })}
+          placeholder="Enter name"
+          helperText={errors.name?.message}
+          error={!!errors.name}
         />
-        <Controller
-          name="email"
-          control={control}
-          rules={{
-            required: true,
-            pattern: patternEmail,
-          }}
-          render={({ field }) => (
-            <TextField
-              placeholder="Enter email"
-              sx={{ mr: 3 }}
-              helperText={errors.email && "Enter a correct email."}
-              error={errors.email ? true : false}
-              {...field}
-            />
-          )}
+        <TextField
+          {...register("email", {
+            required: "Email is required.",
+            pattern: {
+              value: patternEmail,
+              message: "Enter a correct email.",
+            },
+          })}
+          placeholder="Enter email"
+          helperText={errors.email?.message}
+          error={!!errors.email}
         />
-        <Controller
-          name="password"
-          control={control}
-          rules={{
-            required: true,
-            minLength: 6,
-          }}
-          render={({ field }) => (
-            <TextField
-              placeholder="Enter password"
-              sx={{ mr: 3 }}
-              helperText={errors.password && "Enter at least 6 characters."}
-              error={errors.password ? true : false}
-              {...field}
-            />
-          )}
+        <TextField 
+          {...register("password", {
+            required: "Password is required.",
+            minLength: {
+              value: 6,
+              message: "Enter at least 6 characters.",
+            },
+          })}
+          placeholder="Enter password"
+          helperText={errors.password?.message}
+          error={!!errors.password}
         />
-        <Controller
-          name="confirmPassword"
-          control={control}
-          rules={{
-            required: true,
-            validate: () => passwordValue == confirmPasswordValue,
-          }}
-          render={({ field }) => (
-            <TextField
-              placeholder="Repeat password"
-              sx={{ mr: 3 }}
-              helperText={
-                errors.confirmPassword && "The passwords don't match."
+        <TextField
+          {...register("confirmPassword", {
+            required: "Confirm password is required.",
+            validate: (confirmPassword) => {
+              if (passwordValue !== confirmPassword) {
+                return "The passwords don't match.";
               }
-              error={errors.confirmPassword ? true : false}
-              {...field}
-            />
-          )}
-        />
+              
+              return true;
+            },
+          })}
+          placeholder="Repeat password"
+          helperText={
+            errors.confirmPassword?.message
+          }
+          error={!!errors.confirmPassword}
+        />  
         <Controller
           name="role"
           control={control}
@@ -117,36 +106,34 @@ export const RegistrationForm = () => {
             required: true,
           }}
           render={({ field }) => (
-            <Select sx={{ mr: 3 }} label="Role" {...field}>
-              <MenuItem value={"user"}>User</MenuItem>
-              <MenuItem value={"business"}>Business</MenuItem>
-            </Select>
+            <FormControl fullWidth>
+              <InputLabel id="role-select-label">Role</InputLabel>
+              <Select
+                labelId="role-select-label"
+                id="role-select-label"
+                label="Age"
+                {...field}
+              >
+                <MenuItem value={"user"}>User</MenuItem>
+                <MenuItem value={"business"}>Business</MenuItem>
+              </Select>
+            </FormControl>
           )}
         />
-        {roleValue === "user" ? (
-          ""
-        ) : (
-          <Controller
-            name="companyName"
-            control={control}
-            rules={{
+        {roleValue === "business" && (
+          <TextField
+            {...register("companyName", {
               required: "Enter company name",
-            }}
-            render={({ field }) => (
-              <TextField
-                placeholder="Enter company name"
-                sx={{ mr: 3 }}
-                helperText={errors.companyName && errors.companyName.message}
-                error={errors.companyName ? true : false}
-                {...field}
-              />
-            )}
+            })}
+            placeholder="Enter company name"
+            helperText={errors.companyName?.message}
+            error={!!errors.companyName}
           />
         )}
-        <Button type="submit" variant="outlined" sx={{ height: 56 }}>
+        <Button type="submit" variant="outlined">
           Submit
         </Button>
-      </form>
+      </Box>
     </>
   );
 };
